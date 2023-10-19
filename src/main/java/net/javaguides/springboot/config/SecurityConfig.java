c847325937dd5fd64d2f61fa0146028d60c24259
White Tree <whitetree@Whitetreecodiinggalaxy.local>
2023-10-19 17:17:31 +0800

    tom_add_security

---
+++ src/main/java/net/javaguides/springboot/config/SecurityConfig.java
@@ -0,0 +1,30 @@
+package net.javaguides.springboot.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import   org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+
+@RequiredArgsConstructor
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .sessionManagement(customizer -> customizer.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests((requests) -> requests.antMatchers(HttpMethod.POST, "/api/v1/verification").permitAll()
+                        .anyRequest().authenticated())
+        ;
+        return http.build();
+    }
+}
